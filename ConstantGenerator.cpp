f4fd7f0cbe6fd8816d3eb631ae04fc5dc06fcb68
CvetkovKonstantin <praimrprrt@gmail.com>
2026-09-25 15:23:52 +0300

    ConstantGenerator.cpp

---
+++ ConstantGenerator.cpp
@@ -0,0 +1,17 @@
+#include "ConstantGenerator.h"
+
+namespace miit::algebra {
+
+    ConstantGenerator::ConstantGenerator(const int value)
+        : constantValue(value) {
+    }
+
+    int ConstantGenerator::generate() { return constantValue; }
+
+    void ConstantGenerator::setValue(const int value) {
+        constantValue = value;
+    }
+
+    int ConstantGenerator::getValue() const { return constantValue; }
+
+}
