796d763da0811764b63422410c4e621436ddc0d2
CvetkovKonstantin <praimrprrt@gmail.com>
2026-09-25 15:31:21 +0300

    ConstantGenerator.h

---
+++ ConstantGenerator.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Generator.h"
+
+namespace miit::algebra {
+
+    /**
+     * @brief Генератор, возвращающий константное значение
+     */
+    class ConstantGenerator : public Generator {
+    private:
+        int constantValue;
+
+    public:
+        ConstantGenerator(const int value = 0);
+
+        int generate() override;
+
+        void setValue(const int value);
+        int getValue() const;
+    };
+
+}
