81f0bd6a9dcaec81e60ef78874607442f50b8494
CvetkovKonstantin <praimrprrt@gmail.com>
2026-09-25 15:24:53 +0300

    Exercise.cpp

---
+++ Exercise.cpp
@@ -0,0 +1,22 @@
+#include "Exercise.h"
+#include <utility>
+
+namespace miit::algebra {
+
+    void Exercise::setMatrix(const Matrix<int>& mat) {
+        matrix = mat;
+    }
+
+    const Matrix<int>& Exercise::getMatrix() const {
+        return matrix;
+    }
+
+    void Exercise::setGenerator(std::unique_ptr<Generator> gen) {
+        generator = std::move(gen);
+    }
+
+    Generator* Exercise::getGenerator() const {
+        return generator.get();
+    }
+
+}
